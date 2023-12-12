5b43b03bf892e248919e88f793a276c3535a15fb
Gonçalo Maciel <goncalomaciel2250@gmail.com>
2023-12-12 19:56:10 +0000

    swagger on :pig:

--- Controllers/serviceController.js
+++ Controllers/serviceController.js
@@ -2,6 +2,48 @@ const Service = require('../Models/services');
 const Services = require('../Models/services');
 const mongoose = require('mongoose');
 
+/**
+ * @swagger
+ * /services/create:
+ *   post:
+ *     summary: Create a new service
+ *     description: Endpoint to create a new service.
+ *     tags:
+ *       - Services
+ *     requestBody:
+ *       description: Service creation data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *             required:
+ *               - name
+ *               - price
+ *     responses:
+ *       '200':
+ *         description: Service created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 Services:
+ *                   type: object
+ *                   // Define your service properties here
+ *       '400':
+ *         description: Bad Request - Invalid or missing input data
+ *       '500':
+ *         description: Internal Server Error - Failed to create the service
+ */
 exports.CreateService = async (req, res) => {
     const {name, price} = req.body
 
@@ -16,6 +58,54 @@ exports.CreateService = async (req, res) => {
     res.json({status:'success', Services: response})
 }
 
+/**
+ * @swagger
+ * /services/editName:
+ *   put:
+ *     summary: Edit the name of a service
+ *     description: Endpoint to edit the name of an existing service.
+ *     tags:
+ *       - Services
+ *     parameters:
+ *       - name: id
+ *         in: query
+ *         description: ID of the service to edit the name
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Service name editing data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               newName:
+ *                 type: string
+ *             required:
+ *               - newName
+ *     responses:
+ *       '200':
+ *         description: Service name edited successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 Service:
+ *                   type: object
+ *                   // Define your service properties here
+ *       '400':
+ *         description: Bad Request - Invalid or missing input data
+ *       '404':
+ *         description: Not Found - Service not found
+ *       '500':
+ *         description: Internal Server Error - Failed to edit the service name
+ */
 exports.EditServiceName = async (req, res) => {
     try {
         const { newName } = req.body;
@@ -47,6 +137,54 @@ exports.EditServiceName = async (req, res) => {
     }
 };
 
+/**
+ * @swagger
+ * /services/editPrice:
+ *   put:
+ *     summary: Edit the price of a service
+ *     description: Endpoint to edit the price of an existing service.
+ *     tags:
+ *       - Services
+ *     parameters:
+ *       - name: serviceID
+ *         in: query
+ *         description: ID of the service to edit the price
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Service price editing data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               newPrice:
+ *                 type: number
+ *             required:
+ *               - newPrice
+ *     responses:
+ *       '200':
+ *         description: Service price edited successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 service:
+ *                   type: object
+ *                   // Define your service properties here
+ *       '400':
+ *         description: Bad Request - Invalid or missing input data
+ *       '404':
+ *         description: Not Found - Service not found
+ *       '500':
+ *         description: Internal Server Error - Failed to edit the service price
+ */
 exports.EditServicePrice = async (req, res) => {
     try {
         const { newPrice } = req.body;
@@ -73,6 +211,42 @@ exports.EditServicePrice = async (req, res) => {
     }
 };
 
+/**
+ * @swagger
+ * /services/remove:
+ *   delete:
+ *     summary: Remove a service
+ *     description: Endpoint to remove an existing service.
+ *     tags:
+ *       - Services
+ *     parameters:
+ *       - name: serviceID
+ *         in: query
+ *         description: ID of the service to remove
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Service removed successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 service:
+ *                   type: object
+ *                   // Define your service properties here
+ *       '400':
+ *         description: Bad Request - Invalid service ID
+ *       '404':
+ *         description: Not Found - Service not found
+ *       '500':
+ *         description: Internal Server Error - Failed to remove the service
+ */
 exports.RemoveService = async (req, res) => {
     const { serviceID } = req.query;
 
@@ -91,6 +265,42 @@ exports.RemoveService = async (req, res) => {
     res.json({ status: 'success', service: service});
 }
 
+/**
+ * @swagger
+ * /services/read:
+ *   get:
+ *     summary: Get service information
+ *     description: Endpoint to retrieve information for an existing service.
+ *     tags:
+ *       - Services
+ *     parameters:
+ *       - name: serviceID
+ *         in: query
+ *         description: ID of the service to retrieve information
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Service information retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 service:
+ *                   type: object
+ *                   // Define your service properties here
+ *       '400':
+ *         description: Bad Request - Invalid service ID
+ *       '404':
+ *         description: Not Found - Service not found
+ *       '500':
+ *         description: Internal Server Error - Failed to retrieve service information
+ */
 exports.ReadService = async (req, res) => {
     const { serviceID } = req.query;
 
@@ -106,6 +316,33 @@ exports.ReadService = async (req, res) => {
     res.json({ status: 'success', service: service})
 }
 
+/**
+ * @swagger
+ * /services/readAll:
+ *   get:
+ *     summary: Get all services
+ *     description: Endpoint to retrieve information for all existing services.
+ *     tags:
+ *       - Services
+ *     responses:
+ *       '200':
+ *         description: All services retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 services:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     // Define your service properties here
+ *       '500':
+ *         description: Internal Server Error - Failed to retrieve services
+ */
 exports.ReadServices = async (req, res) => {
     const services = await Services.find();
     res.json({ status: 'success', services: services })
---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API',
+      version: '1.0.0',
+      description: 'A simple API with Swagger documentation',
+    },
+  },
+  apis: ['./apiServices/Controllers/serviceController.js'], // Point to the file that contains your route definitions
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = {
+  specs,
+  swaggerUi,
+};
